166afeb9497b58320887528b2efca92595ccf276
RIccardoIzzo <izzo.riccardo38@gmail.com>
2021-03-21 14:58:09 +0100

    create packages model and controller

---
+++ src/test/java/it/polimi/ingsw/controller/ControllerTest.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.controller;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+
+}
---
+++ src/test/java/it/polimi/ingsw/model/GameTest.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.model;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+}
