ce0fbe74ccd260bc871175f4f305e07981662f0c
GabrieleLazzarelli <gabriele.lazzarelli@mail.polimi.it>
2021-03-26 10:54:44 +0100

    add Requirement interface

---
+++ src/main/java/it/polimi/ingsw/model/Requirement.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.model;
+
+/**
+ * Interface Requirement has to be implemented by every Card in the game.
+ * It counts as a cost for the DevelopmentCard or as a requirement for the LeaderCard.
+ * Marker interface pattern design is applied.
+ */
+public interface Requirement {
+}
