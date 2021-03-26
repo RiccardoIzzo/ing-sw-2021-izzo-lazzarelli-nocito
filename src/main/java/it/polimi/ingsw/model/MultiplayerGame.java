fc75cfab70ab47fb3352d2ddd0bc738bc08b5c21
RIccardoIzzo <izzo.riccardo38@gmail.com>
2021-03-26 12:09:35 +0100

    add: MultiplayerGame and MultiplayerGameTest javadoc

--- src/main/java/it/polimi/ingsw/model/MultiplayerGame.java
+++ src/main/java/it/polimi/ingsw/model/MultiplayerGame.java
@@ -3,39 +3,72 @@ package it.polimi.ingsw.model;
 import java.util.ArrayList;
 import java.util.Random;
 
-
+/**
+ * MultiplayerGame class extends Game class and implements the logic of a multiplayer match up to four players.
+ *
+ * @author Riccardo Izzo
+ */
 public class MultiplayerGame extends Game{
     private ArrayList<Player> players = new ArrayList<>();
     private Player currPlayer;
     private Player firstPlayer;
     private int playerIndex;
 
+    /**
+     * Method getCurrPlayer returns the current player.
+     * @return the current player.
+     */
     public Player getCurrPlayer(){
         return currPlayer;
     }
 
+    /**
+     * Method setCurrPlayer sets the current player.
+     * @param player the new current player to be set.
+     */
     public void setCurrPlayer(Player player){
         currPlayer = player;
     }
 
+    /**
+     * Method getFirstPlayer return the first player, the one with the inkwell.
+     * @return the first player.
+     */
     public Player getFirstPlayer(){
         return firstPlayer;
     }
 
+    /**
+     * Method setFirstPlayer sets the first player at the beginning of the game.
+     * The first player is randomly chosen from the list of players.
+     */
     public void setFirstPlayer(){
         playerIndex = new Random().nextInt(getNumPlayers());
         setCurrPlayer(players.get(playerIndex));
         firstPlayer = currPlayer;
     }
 
+    /**
+     * Method getNumPlayers returns the number of players.
+     * @return the number of players.
+     */
     public int getNumPlayers(){
         return players.size();
     }
 
+    /**
+     * Method getPlayers return the list of players.
+     * @return a list of players.
+     */
     public ArrayList<Player> getPlayers(){
         return players;
     }
 
+    /**
+     * Method getPlayerByName return a player instance relying on his name.
+     * @param name player name.
+     * @return an instance of player.
+     */
     public Player getPlayerByName(String name){
         for(Player player : players){
             if(player.getName().equals(name)){
@@ -45,14 +78,25 @@ public class MultiplayerGame extends Game{
         return null;
     }
 
+    /**
+     * Method addPlayer create a nem player and adds it to the list of player.
+     * @param name name of the player to be added.
+     */
     public void addPlayer(String name){
         players.add(new Player(name));
     }
 
+    /**
+     * Method removePlayer remove a player from the list.
+     * @param name name of the player to be removed.
+     */
     public void removePlayer(String name){
         players.remove(getPlayerByName(name));
     }
 
+    /**
+     * Method nextPlayer updates the current player after the turn.
+     */
     public void nextPlayer(){
         if(playerIndex == players.size() - 1){
             playerIndex = 0;
--- src/test/java/it/polimi/ingsw/model/MultiplayerGameTest.java
+++ src/test/java/it/polimi/ingsw/model/MultiplayerGameTest.java
@@ -2,14 +2,21 @@ package it.polimi.ingsw.model;
 
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
+/**
+ * MultiplayerGameTest tests MultiplayerGame class.
+ *
+ * @author Riccardo Izzo
+ */
 public class MultiplayerGameTest {
     MultiplayerGame game;
 
+    /**
+     * Method initialization create an instance of MultiplayerGame, adds three players and choose randomly the first player.
+     */
     @Before
-    public void setUp(){
+    public void initialization(){
         game = new MultiplayerGame();
         game.addPlayer("Riccardo");
         game.addPlayer("Andrea");
@@ -17,6 +24,9 @@ public class MultiplayerGameTest {
         game.setFirstPlayer();
     }
 
+    /**
+     * Method AddRemovePlayers tests addPlayer and removePlayer methods, checks the number of players.
+     */
     @Test
     public void AddRemovePlayers(){
         assertEquals("Riccardo", game.getPlayers().get(0).getName());
@@ -33,6 +43,9 @@ public class MultiplayerGameTest {
         assertEquals(0, game.getNumPlayers());
     }
 
+    /**
+     * Method playerRotation tests the clockwise rotation of the players during the game.
+     */
     @Test
     public void playerRotation(){
         assertEquals(game.getCurrPlayer(), game.getFirstPlayer());
