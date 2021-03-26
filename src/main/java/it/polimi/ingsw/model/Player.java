13be5535392834b020cdd4f4e9a6b888e686569b
GabrieleLazzarelli <gabriele.lazzarelli@mail.polimi.it>
2021-03-26 12:36:31 +0100

    add attributes to Player victoryPoints, developments, leaders, availableProduction, availableExchange, availableDiscount

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -1,8 +1,16 @@
 package it.polimi.ingsw.model;
 
+import java.util.Set;
+
 public class Player {
     private String nickname;
     private int victoryPoints;
+    //private Dashboard myDashboard;
+    private Set<Card> developments;
+    private Set<Card> leaders;
+    private Set<ProductionPower> availableProduction;
+    private Set<MarbleColor> availableExchange;
+    private Set<Resource> availableDiscount;
 
     public Player(String name){
         nickname = name;
