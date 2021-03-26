06363d35dcbe586e456b2f2694753e219e7d93c9
GabrieleLazzarelli <gabriele.lazzarelli@mail.polimi.it>
2021-03-26 10:45:53 +0100

    add DevelopmentCard

---
+++ src/main/java/it/polimi/ingsw/model/DevelopmentCard.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw.model;
+
+/**
+ * Class DevelopmentCard contains a ProductionPower which players can use once they have bought one.
+ */
+public class DevelopmentCard extends Card{
+    private final CardColor type;
+    private final int level;
+    private final ProductionPower production;
+
+    /**
+     * Constructor Card takes two parameters which are shared among all of the subclasses.
+     * Constructor DevelopmentCard sets instance's type, level and production.
+     * @param victoryPoints are the card's victory points .
+     * @param requirement is the card's requirement.
+     * @param type is the CardColor type
+     * @param level is an Integer, can vary from one to three
+     * @param production is the ProductionPower associated
+     */
+    public DevelopmentCard(int victoryPoints, Requirement requirement, CardColor type, int level, ProductionPower production) {
+        super(victoryPoints, requirement);
+        this.type = type;
+        this.level = level;
+        this.production = production;
+    }
+
+    /**
+     * Method getType gets the color of the card
+     * @return the CardColor value of the DevelopmentCard
+     */
+    public CardColor getType() {
+        return type;
+    }
+
+    /**
+     * Method getLevel gets the level of DevelopmentCard, can vary from one to three
+     * @return the level of the DevelopmentCard
+     */
+    public int getLevel() {
+        return level;
+    }
+
+    /**
+     * Method ProductionPower gets the productionPower of DevelopmentCard
+     * @return the productionPower of the DevelopmentCard
+     */
+    public ProductionPower getProduction() {
+        return production;
+    }
+}
