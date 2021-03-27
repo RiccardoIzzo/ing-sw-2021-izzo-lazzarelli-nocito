a0c17c91dceecdec62c86bee30803c99385a4902
GabrieleLazzarelli <gabriele.lazzarelli@mail.polimi.it>
2021-03-27 18:04:20 +0100

    add DiscountLeaderCard class

---
+++ src/main/java/it/polimi/ingsw/model/DiscountLeaderCard.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.model;
+
+/**
+ * Class DiscountLeaderCard is a type of LeaderCard which provides the player discount when buying DevelopmentCard.
+ */
+public class DiscountLeaderCard extends LeaderCard{
+    private ResourceMap discount;
+
+    /**
+     * Constructor of the superclass Card takes two parameters which are shared among all of the subclasses.
+     * Constructor LeaderCard sets active=false since at the beginning there are not active LeaderCard.
+     * Constructor of DiscountLeader takes a ResourceMap containing the discount for each Resource.
+     * @param victoryPoints are the card's victory points.
+     * @param requirement   is the card's requirement.
+     * @param discount      is a ResourceMap, each Resource has an Integer associated, the discount value.
+     */
+    public DiscountLeaderCard(int victoryPoints, Requirement requirement, ResourceMap discount) {
+        super(victoryPoints, requirement);
+        this.discount = discount;
+    }
+
+    /**
+     * Method setAbility adds the discount DiscountLeaderCard provides to the list of available discounts
+     */
+    @Override
+    public void setAbility(Player player) {
+        player.addDiscount(discount);
+    }
+}
