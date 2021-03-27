22607c23d3569bbd95e773b0e81c7180a390da94
GabrieleLazzarelli <gabriele.lazzarelli@mail.polimi.it>
2021-03-27 13:25:47 +0100

    add WhiteMarbleLeaderCard class

---
+++ src/main/java/it/polimi/ingsw/model/WhiteMarbleLeaderCard.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.model;
+
+import java.util.Set;
+
+public class WhiteMarbleLeaderCard extends LeaderCard{
+    private final Set<MarbleColor> exchange;
+
+    /**
+     * Constructor Card takes two parameters which are shared among all of the subclasses.
+     * Constructor LeaderCard sets active=false since at the beginning there are not active LeaderCard.
+     * Constructor WhiteMarbleLeaderCard takes a ProductionPower parameter.
+     * @param victoryPoints is the card's victoryPoints.
+     * @param requirement   is the card's Requirement.
+     * @param exchange      is a collection of MarbleColor which can be traded for the WhiteMarble in Market.
+     */
+    public WhiteMarbleLeaderCard(int victoryPoints, Requirement requirement, Set<MarbleColor> exchange) {
+        super(victoryPoints, requirement);
+        this.exchange = exchange;
+    }
+
+    public Set<MarbleColor> getExchange() {
+        return exchange;
+    }
+}
