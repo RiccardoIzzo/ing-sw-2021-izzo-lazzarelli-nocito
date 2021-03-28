73effae7fa5e2ca0b9b35eb0e31d0d3fba8457e8
RIccardoIzzo <izzo.riccardo38@gmail.com>
2021-03-28 20:31:02 +0200

    add: SoloActionToken interface

---
+++ src/main/java/it/polimi/ingsw/model/SoloActionToken.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.model;
+
+/**
+ * Interface SoloActionToken has to be implemented by the two types of token, MoveBlackMarker and RemoveCards.
+ * It generalizes the concept of token.
+ *
+ * @author Riccardo Izzo
+ */
+
+public interface SoloActionToken {
+    /**
+     * Method playToken has to be implemented by both types of tokens and represents the activation of a token.
+     */
+    void playToken();
+}
