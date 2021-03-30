7bb2c28fc93caf81c660ac8ecc5c05b7028d8b81
GabrieleLazzarelli <gabriele.lazzarelli@mail.polimi.it>
2021-03-30 20:47:22 +0200

    add addCard method to CardMap

--- src/main/java/it/polimi/ingsw/model/CardMap.java
+++ src/main/java/it/polimi/ingsw/model/CardMap.java
@@ -36,15 +36,25 @@ public class CardMap {
     }
 
     /**
+     * Method addCard increments the specified CardColor value of this CardMap
+     * @param cardColor is the CardColor key which value is to increment
+     * @param value the amount to increment
+     */
+    public void addCard(CardColor cardColor, int value) {
+        int toIncrement = this.getCard(cardColor);
+        this.put(cardColor, value + toIncrement);
+    }
+
+    /**
      * Methods addCards takes another CardMap as input and for each CardColor key increases this CardMap value
      * by the value in CardMap parameter.
      * @param cardMap is a CardMap, contains the values to add to this CardMap
      */
     public void addCards(CardMap cardMap){
-        for (CardColor value : CardColor.values()) {
-            int toIncrement = this.getCard(value);
-            int increment = cardMap.getCard(value);
-            this.put(value, toIncrement + increment);
+        for (CardColor cardColor : CardColor.values()) {
+            int toIncrement = this.getCard(cardColor);
+            int increment = cardMap.getCard(cardColor);
+            this.put(cardColor, toIncrement + increment);
         }
     }
 
@@ -52,13 +62,13 @@ public class CardMap {
      * Method flush sets each CardColor key of this CardMap to 0.
      */
     public void flush(){
-        for (CardColor value : CardColor.values()) {
-            cards.put(value, 0);
+        for (CardColor cardColor : CardColor.values()) {
+            cards.put(cardColor, 0);
         }
     }
 
     /**
-     * Method put sets a value to a specified CardColor of this CardMap.
+     * Method addCard sets a value to a specified CardColor of this CardMap.
      * @param cardColor is the CardColor key to set
      * @param value is the value to set to the specified CardColor key
      */
