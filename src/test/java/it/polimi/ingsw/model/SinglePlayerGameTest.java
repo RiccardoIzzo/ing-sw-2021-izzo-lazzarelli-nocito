1bb34f6b6ed5f8f8106b60ae7ac8ed8cf563d0ec
RIccardoIzzo <izzo.riccardo38@gmail.com>
2021-03-30 16:31:22 +0200

    update tests of game classes after uml changes

---
+++ src/test/java/it/polimi/ingsw/model/SinglePlayerGameTest.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * SinglePlayerGameTest tests SinglePlayerGame class.
+ *
+ * @author Riccardo Izzo
+ */
+public class SinglePlayerGameTest {
+    SinglePlayerGame singlePlayerGame;
+
+    /**
+     * Method initialization create an instance of SinglePlayerGame.
+     */
+    @Before
+    public void initialization(){
+        singlePlayerGame = new SinglePlayerGame();
+    }
+}
