e16c9243db59e0c22739c2899c6e8a8dcf70335b
Andrea <andrea.nocito@mail.polimi.it>
2021-04-06 15:27:36 +0200

    update getNumberOfActiveTiles into getPointsForTiles in FaithTrack

--- src/main/java/it/polimi/ingsw/model/player/FaithTrack.java
+++ src/main/java/it/polimi/ingsw/model/player/FaithTrack.java
@@ -16,10 +16,13 @@ public class FaithTrack {
     private static final int[] WINNING_VALUES = {1,2,4,6,9,12,16,20};
     // TILE_POS : position of tiles in the game.
     private static final int[] TILE_POS = {8,16,24};
+    // POINTS_FOR_TILE : position of tiles in the game.
+    private static final int[] POINTS_FOR_TILE = {2,3,4};
     // INITIAL OFFSET: offset from the first tile which defines the "point zone"
     private static final int INITIAL_OFFSET = 4;
     // END: last tile of the game
     static final int END = 24;
+
 
     public FaithTrack() {
         posFaithMarker = 0;
@@ -41,10 +44,10 @@ public class FaithTrack {
     }
     public void popeTilePass() {
     }
-    public int getNumberOfActiveTiles() {
+    public int getPointsForTiles() {
         int counter = 0;
-        for (Boolean tile : popesFavorTiles) {
-            counter += tile ? 1 : 0;
+        for (int i = 0; i < popesFavorTiles.length; i++) {
+            counter += popesFavorTiles[i] ? POINTS_FOR_TILE[i] : 0;
         }
         return counter;
     }
@@ -55,7 +58,7 @@ public class FaithTrack {
             victoryPoints += WINNING_VALUES[i];
             i++;
         }
-        return victoryPoints;
+        return victoryPoints+getPointsForTiles();
     }
     public int getPlayerPos() {
         return posFaithMarker;
