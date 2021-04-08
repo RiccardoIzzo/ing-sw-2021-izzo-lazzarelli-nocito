8f6213d36915933fa50ae3dbc4c3625092fc9b7f
Andrea <andrea.nocito@mail.polimi.it>
2021-04-08 19:44:35 +0200

    update Dashboard methods description

--- src/main/java/it/polimi/ingsw/model/player/Dashboard.java
+++ src/main/java/it/polimi/ingsw/model/player/Dashboard.java
@@ -21,13 +21,25 @@ public class Dashboard {
     void useAbility() {
 
     }
+
+    /**
+     * Method addShelf receives a shelf and adds it to the list
+     */
     public void addShelf(Shelf shelf) {
         warehouse.addShelf(shelf);
     }
+
+    /**
+     * Method addResource adds one resource into the strongBox
+     */
     void addResource(Resource resource) {
         strongBox.addResource(resource, 1);
 
     }
+
+    /**
+     * Method incrementFaith receives the number of steps to take and makes the path move forward.
+     */
     void incrementFaith(int steps) {
         for(int i=0; i<steps; i++) {
             path.moveForward();
@@ -38,6 +50,9 @@ public class Dashboard {
         return path;
     }
 
+    /**
+     * Method incrementBlackFaith makes the path move the black cross forward.
+     */
 
     public void incrementBlackFaith(){
         ((SinglePlayerFaithTrack) path).moveBlack();
--- src/test/java/it/polimi/ingsw/model/ResourceMapTest.java
+++ src/test/java/it/polimi/ingsw/model/ResourceMapTest.java
@@ -1,7 +1,7 @@
 package it.polimi.ingsw.model;
 
 import org.junit.Before;
-import org.junit.Test;
+import org.testng.annotations.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
