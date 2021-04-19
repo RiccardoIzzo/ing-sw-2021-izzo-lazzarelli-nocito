e961da9d5275a4d040ba5ace3766f251b72589e6
GabrieleLazzarelli <gabriele.lazzarelli@mail.polimi.it>
2021-04-19 16:25:00 +0200

    Add ExtraShelfLeaderCardTest

---
+++ src/test/java/it/polimi/ingsw/model/card/ExtraShelfLeaderCardTest.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.model.card;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * This class tests if:
+ * @author Gabriele Lazzarelli
+ */
+public class ExtraShelfLeaderCardTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
