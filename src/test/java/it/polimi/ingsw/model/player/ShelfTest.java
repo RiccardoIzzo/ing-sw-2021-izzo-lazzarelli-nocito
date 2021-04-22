4ac96f5d6cf6d1dff3e1ed0c2b83471e35bbf147
Andrea <andrea.nocito@mail.polimi.it>
2021-04-22 18:22:10 +0200

    update methods of ShelfTest with comments

--- src/test/java/it/polimi/ingsw/model/player/ShelfTest.java
+++ src/test/java/it/polimi/ingsw/model/player/ShelfTest.java
@@ -29,11 +29,18 @@ public class ShelfTest {
         shelf = new Shelf(2, resourcesAllowedTest);
     }
 
+    /**
+     * Method testGetResourceAllowed checks that the resources allowed inside the shelf are the ones set in at initializations
+     */
     @Test
     public void testGetResourceAllowed() {
         assertEquals(shelf.getResourceAllowed(), resourcesAllowedTest);
     }
 
+    /**
+     * Method testTakeResource places one COIN resource and tries to take one resource for each type
+     * Checks that only the method with COIN as a parameter returns a positive value.
+     */
     @Test
     public void testTakeResource() {
         assertTrue(shelf.placeResource(Resource.COIN));
@@ -43,7 +50,11 @@ public class ShelfTest {
         assertFalse(shelf.takeResource(Resource.STONE));
 
     }
-
+    /**
+     * Method testPlaceResources checks the placeResources method:
+     * First it tries add 3 resources to the shelf, and checks that the operation return a negative response, since the capacity of the shelf is set to 2.
+     * After it tries to add 2 resource to the shelf, successfully, removes them one by one and check that is not possible to remove another one from the empty shelf.
+     */
     @Test
     public void testPlaceResources() {
         ResourceMap resourcesTest = new ResourceMap();
@@ -56,6 +67,9 @@ public class ShelfTest {
         assertFalse(shelf.takeResource(Resource.COIN));
     }
 
+    /**
+     * Method testPlaceResource tries to place one of each type of Resources, and checks that only COIN gets added to shelf, since it is the only Resource type allowed.
+     */
     @Test
     public void testPlaceResource() {
         assertTrue(shelf.placeResource(Resource.COIN));
