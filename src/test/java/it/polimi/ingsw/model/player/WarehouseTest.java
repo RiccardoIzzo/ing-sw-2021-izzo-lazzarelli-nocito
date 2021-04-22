874fdd9e3de3f9d8bc96d02b07b8caef20f5abb2
Andrea <andrea.nocito@mail.polimi.it>
2021-04-22 18:30:54 +0200

    add WarehouseTest

---
+++ src/test/java/it/polimi/ingsw/model/player/WarehouseTest.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw.model.player;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+public class WarehouseTest extends TestCase {
+    public void testAddShelf() {
+    }
+
+    public void testAddResource() {
+    }
+
+    public void testGetShelvesSize() {
+    }
+
+    public void testGetNumberShelf() {
+    }
+
+    public void testAddResourcesIntoTemporaryShelf() {
+    }
+
+    public void testRemoveResourcesFromTemporaryShelf() {
+    }
+
+    public void testRemoveResource() {
+    }
+
+    public void testSwapResource() {
+    }
+}
