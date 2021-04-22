29b22cbd0a109e18956828ce739889edcc691022
Andrea <andrea.nocito@mail.polimi.it>
2021-04-22 17:02:13 +0200

    add ShelfTest

---
+++ src/test/java/it/polimi/ingsw/model/player/ShelfTest.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.model.player;
+
+import junit.framework.TestCase;
+
+public class ShelfTest extends TestCase {
+
+    public void testGetResourceAllowed() {
+    }
+
+    public void testTakeResource() {
+    }
+
+    public void testPlaceResources() {
+    }
+
+    public void testPlaceResource() {
+    }
+}
