bab6f4fe4c90d50bf940d1eeca7dd4defaabc89a
Andrea <andrea.nocito@mail.polimi.it>
2021-04-23 16:55:13 +0200

    add FaithTrackTest

---
+++ src/test/java/it/polimi/ingsw/model/player/FaithTrackTest.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.model.player;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+/**
+ * FaithTrackTest tests FaithTrack class.
+ *
+ * @author Andrea Nocito
+ */
+
+public class FaithTrackTest{
+
+    public void testMoveForward() {
+    }
+
+    public void testIsInVaticanSpace() {
+    }
+
+    public void testPopeTilePass() {
+    }
+
+    public void testGetPointsForTiles() {
+    }
+
+    public void testGetPosVictoryPoints() {
+    }
+
+    public void testGetPlayerPos() {
+    }
+}
