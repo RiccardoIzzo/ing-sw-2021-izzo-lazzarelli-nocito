8ddec6635356e120b3d4988af6b577199f04e5f7
Andrea <andrea.nocito@mail.polimi.it>
2021-04-23 22:11:45 +0200

    update DashboardTest

--- src/test/java/it/polimi/ingsw/model/player/DashboardTest.java
+++ src/test/java/it/polimi/ingsw/model/player/DashboardTest.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.model.player;
 
-import org.junit.Assert;
+import it.polimi.ingsw.model.Resource;
+import it.polimi.ingsw.model.ResourceMap;
+
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -20,13 +22,37 @@ public class DashboardTest {
      * Method testAddShelf tests addPlayer and removePlayer methods, checks the number of players.
      */
     @Test
-    public void addShelfTest() {
+    public void testAddShelf() {
         final int numberOfShelfAdded = 3;
         for(int i=1; i<numberOfShelfAdded+1; i++) {
             dashboard.addShelf(new Shelf(i));
         }
         Warehouse warehouse = dashboard.getWarehouse();
-        Assert.assertEquals(warehouse.getNumberShelf() + numberOfShelfAdded, warehouse.getShelvesSize());
+        assertEquals(warehouse.getNumberShelf() + numberOfShelfAdded, warehouse.getShelvesSize());
+    }
+    /**
+     * Method testAddResource adds one resource in the strongbox and checks if it is there
+     */
+    @Test
+    public void testAddResource() {
+        dashboard.addResource(Resource.COIN);
+        ResourceMap strongBox = dashboard.getStrongBox();
+        int resValue = strongBox.getResource(Resource.COIN);
+        assertEquals( resValue,1);
     }
 
+    /**
+     * Method testRemoveResource  adds one resource in the strongbox, then removes it and checks that strongbox is empty
+     */
+    @Test
+    public void testRemoveResource() {
+        dashboard.addResource(Resource.COIN);
+        ResourceMap strongBox = dashboard.getStrongBox();
+        int resValue = strongBox.getResource(Resource.COIN);
+        assertEquals( resValue,1);
+        assertTrue(dashboard.removeResource(Resource.COIN));
+        strongBox = dashboard.getStrongBox();
+        resValue = strongBox.getResource(Resource.COIN);
+        assertEquals( resValue,0);
+    }
 }
