6ec83efafbc9472d6f975e5665fb4876e81b96fc
RIccardoIzzo <izzo.riccardo38@gmail.com>
2021-04-27 18:10:28 +0200

    add several constants

--- src/main/java/it/polimi/ingsw/constants/GameConstants.java
+++ src/main/java/it/polimi/ingsw/constants/GameConstants.java
@@ -1,4 +1,17 @@
 package it.polimi.ingsw.constants;
 
 public class GameConstants {
+    public static final int NUM_DEVELOPMENT_CARDS = 48;
+    public static final int NUM_LEADER_CARDS = 16;
+    public static final int NUM_TOKENS = 7;
+    public static final String developmentCardsJson = "src/main/resources/json/development_card.json";
+    public static final String[] leaderCardsJson = {
+            "src/main/resources/json/discount_leadercard.json",
+            "src/main/resources/json/extrashelf_leadercard.json",
+            "src/main/resources/json/production_leadercard.json",
+            "src/main/resources/json/whitemarble_leadercard.json"
+    };
+    public static final int NUM_WHITE_MARBLES = 4;
+    public static final int NUM_MARBLES = 2;
+    public static final int NUM_RED_MARBLES = 1;
 }
