100ce8e26af781544439e950a8309677239246d3
RIccardoIzzo <izzo.riccardo38@gmail.com>
2021-05-03 16:27:39 +0200

    add Disconnection/Heartbeat/SetPlayersNumber client messages

---
+++ src/main/java/it/polimi/ingsw/events/clientmessages/Disconnection.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.events.clientmessages;
+
+/**
+ * Disconnection message is used to notify a client disconnection.
+ */
+public class Disconnection implements ClientMessage{}
---
+++ src/main/java/it/polimi/ingsw/events/clientmessages/Heartbeat.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.events.clientmessages;
+
+/**
+ * Heartbeat message is used to notify the server that the client-side connection is alive and responsive.
+ */
+public class Heartbeat implements ClientMessage{}
