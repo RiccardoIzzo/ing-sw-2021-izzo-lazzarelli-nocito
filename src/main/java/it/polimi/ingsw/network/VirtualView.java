0055c3cf9940d02eed2a6a000016a67d137cb45f
GabrieleLazzarelli <gabriele.lazzarelli@mail.polimi.it>
2021-05-05 19:20:37 +0200

    add empty VirtualView class

---
+++ src/main/java/it/polimi/ingsw/network/VirtualView.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.network;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class VirtualView implements PropertyChangeListener {
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+}
