3c90160bef883bdcfbd32cdd10f3756396a98bd5
RIccardoIzzo <izzo.riccardo38@gmail.com>
2021-05-07 19:36:00 +0200

    fix ValidNickname message javadoc

--- src/main/java/it/polimi/ingsw/events/servermessages/ValidNickname.java
+++ src/main/java/it/polimi/ingsw/events/servermessages/ValidNickname.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.events.servermessages;
 
 /**
- * Method ValidNickname is used to notify the player that the chosen nickname is valid.
+ * ValidNickname message is used to notify the player that the chosen nickname is valid.
  */
 public class ValidNickname implements ServerMessage {}
