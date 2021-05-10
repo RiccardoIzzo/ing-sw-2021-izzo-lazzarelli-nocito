a5793850b02e920987f669a1b814853e8ede040e
RIccardoIzzo <izzo.riccardo38@gmail.com>
2021-05-10 23:29:43 +0200

    fix javadoc in InvalidNickname message

--- src/main/java/it/polimi/ingsw/events/servermessages/InvalidNickname.java
+++ src/main/java/it/polimi/ingsw/events/servermessages/InvalidNickname.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.events.servermessages;
 
 /**
- * Method InvalidNickname is used to notify the player that the chosen nickname is not valid.
+ * InvalidNickname message is used to notify the player that the chosen nickname is not valid.
  */
 public class InvalidNickname implements ServerMessage{}
