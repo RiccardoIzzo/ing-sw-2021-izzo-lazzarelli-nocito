7a8b5166c8cc97a446b9842563dcdcb238b73f9d
Andrea <andrea.nocito@mail.polimi.it>
2021-05-10 16:19:54 +0200

    add FaithTrackListener class

---
+++ src/main/java/it/polimi/ingsw/listeners/FaithTrackListener.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.listeners;
+
+import it.polimi.ingsw.network.VirtualView;
+
+import java.beans.PropertyChangeEvent;
+
+
+public class FaithTrackListener extends Listener{
+    public FaithTrackListener(VirtualView virtualView) {
+        super(virtualView);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+    }
+}
