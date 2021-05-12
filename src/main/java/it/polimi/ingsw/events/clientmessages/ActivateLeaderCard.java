11113a7949ba4a4bb48c68525111bdbd045f97dc
RIccardoIzzo <izzo.riccardo38@gmail.com>
2021-05-12 23:22:55 +0200

    fix ActivateLeaderCard message

--- src/main/java/it/polimi/ingsw/events/clientmessages/ActivateLeaderCard.java
+++ src/main/java/it/polimi/ingsw/events/clientmessages/ActivateLeaderCard.java
@@ -4,21 +4,21 @@ package it.polimi.ingsw.events.clientmessages;
  * ActivateLeaderCard message is used by the player to activate a leader card.
  */
 public class ActivateLeaderCard implements ClientMessage{
-    private final int index;
+    private final int cardID;
 
     /**
      * Constructor ActivateLeaderCard creates a new ActivateLeaderCard instance.
-     * @param index index of the leader card to activate.
+     * @param cardID id of the leader card to activate.
      */
-    public ActivateLeaderCard(int index){
-        this.index = index;
+    public ActivateLeaderCard(int cardID){
+        this.cardID = cardID;
     }
 
     /**
-     * Method getIndex returns the index of the selected leader card.
-     * @return the index of the select leader card.
+     * Method getIndex returns the id of the selected leader card.
+     * @return the id of the leader card.
      */
-    public int getIndex() {
-        return index;
+    public int getCardID() {
+        return cardID;
     }
 }
