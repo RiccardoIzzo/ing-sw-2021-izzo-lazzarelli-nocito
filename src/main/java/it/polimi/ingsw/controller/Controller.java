6b4d1d0c25460d49e6525c37e18caf31f42445fc
Andrea <andrea.nocito@mail.polimi.it>
2021-05-12 19:10:46 +0200

    add Controller class

---
+++ src/main/java/it/polimi/ingsw/controller/Controller.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.controller;
+
+import it.polimi.ingsw.model.Game;
+
+public class Controller {
+    Game game;
+    GameHandler gameHandler;
+    TurnHandler turnHandler;
+
+    public Controller() {
+
+    }
+    public void start() {
+
+    }
+
+}
