592a07f2bd3a96b93a61f59032a4b01d88f7f596
Andrea <andrea.nocito@mail.polimi.it>
2021-05-12 23:27:24 +0200

    update Controller class

--- src/main/java/it/polimi/ingsw/controller/Controller.java
+++ src/main/java/it/polimi/ingsw/controller/Controller.java
@@ -1,17 +1,26 @@
 package it.polimi.ingsw.controller;
 
+import it.polimi.ingsw.events.servermessages.*;
+import it.polimi.ingsw.network.Server;
+
 import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.MultiplayerGame;
+import it.polimi.ingsw.model.SinglePlayerGame;
+
 
 public class Controller {
     Game game;
     GameHandler gameHandler;
     TurnHandler turnHandler;
 
+    private Server server;
+
     public Controller() {
-
+        gameHandler = new GameHandler();
+        turnHandler = new TurnHandler();
     }
-    public void start() {
-
+    public void setPlayersNumber(int numberOfPlayers) {
+        game = numberOfPlayers > 1 ? new MultiplayerGame() : new SinglePlayerGame();
     }
 
 }
