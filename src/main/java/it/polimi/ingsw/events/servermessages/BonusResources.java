c979acbacff92eea58dbdc15e24ec24d126f8e22
RIccardoIzzo <izzo.riccardo38@gmail.com>
2021-05-17 17:16:15 +0200

    add BonusResources message (ServerMessage)

---
+++ src/main/java/it/polimi/ingsw/events/servermessages/BonusResources.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.events.servermessages;
+
+/**
+ * AskBonusResources message is used to notify the player that he have to choose some bonus resources due to the play order.
+ */
+public class BonusResources implements ServerMessage{
+    private final int amount;
+
+    /**
+     * Constructor BonusResources creates a new BonusResources instance.
+     * @param amount amount of bonus resources.
+     */
+    public BonusResources(int amount){
+        this.amount = amount;
+    }
+
+    /**
+     * Method getAmount returns the amount of resources to choose.
+     * @return the amount of resources.
+     */
+    public int getAmount(){
+        return amount;
+    }
+}
