93fb34cbba2eb9a632ef20c81d0901d12fa32676
GabrieleLazzarelli <gabriele.lazzarelli@mail.polimi.it>
2021-05-23 17:46:50 +0200

    update CLI game title

--- src/main/java/it/polimi/ingsw/view/CLI.java
+++ src/main/java/it/polimi/ingsw/view/CLI.java
@@ -233,7 +233,14 @@ public class CLI implements View{
      * @param args main args.
      */
     public static void main(String[] args) {
-        System.out.println("MASTERS OF RENAISSANCE - CLI");
+        System.out.println(
+                """
+                 __  __           _             ___   __ ____                  _                             \s
+                |  \\/  | __ _ ___| |_ ___ _ __ / _ \\ / _|  _ \\ ___ _ __   __ _(_)___ ___  __ _ _ __   ___ ___\s
+                | |\\/| |/ _` / __| __/ _ \\ '__| | | | |_| |_) / _ \\ '_ \\ / _` | / __/ __|/ _` | '_ \\ / __/ _ \\
+                | |  | | (_| \\__ \\ ||  __/ |  | |_| |  _|  _ <  __/ | | | (_| | \\__ \\__ \\ (_| | | | | (_|  __/
+                |_|  |_|\\__,_|___/\\__\\___|_|   \\___/|_| |_| \\_\\___|_| |_|\\__,_|_|___/___/\\__,_|_| |_|\\___\\___|
+                """);
         System.out.println("Insert server ip address:");
         String ip = new Scanner(System.in).next();
         System.out.println("Insert server port:");
