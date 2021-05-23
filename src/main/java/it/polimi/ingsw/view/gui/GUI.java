807bd8bce4e8ba7cfab9d89f60a4012f0fae0ac1
Andrea <andrea.nocito@mail.polimi.it>
2021-05-23 17:27:19 +0200

    update GUI with first draft of the connection scene

--- src/main/java/it/polimi/ingsw/view/gui/GUI.java
+++ src/main/java/it/polimi/ingsw/view/gui/GUI.java
@@ -6,9 +6,16 @@ import javafx.application.Application;
 
 import it.polimi.ingsw.events.clientmessages.ClientMessage;
 import it.polimi.ingsw.events.servermessages.ServerMessage;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
+import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
@@ -21,20 +28,39 @@ import java.util.Map;
  * GUI class manages the game with a Graphical User Interface.
  */
 public class GUI extends Application implements View {
+    @FXML Label serverLabel;
+    @FXML TextField serverTextField;
+    @FXML Label ipLabel;
+    @FXML TextField ipTextField;
+    @FXML Label nicknameLabel;
+    @FXML TextField nicknameTextField;
+    @FXML Button setNicknameButton;
+    @FXML Button connectButton;
+    @FXML Group nicknameGroup;
+
 
     @Override
     public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/scenes/scene1.fxml"));
+        Parent root = loader.load();
+
         stage.setTitle("Master of Renaissance");
-        Label label = new Label("lets play");
-        VBox vBox = new VBox();
-        Scene scene = new Scene(vBox, 800, 600);
-        scene.setCursor(Cursor.CROSSHAIR);
-
-
-        stage.setScene(scene);
-
-        stage.setOnCloseRequest((event) -> System.out.println("Closing Stage"));
+        stage.setScene(new Scene(root));
         stage.show();
+
+//        Label label = new Label("lets play");
+//        VBox vBox = new VBox();
+//        Scene scene = new Scene(vBox, 800, 600);
+//        scene.setCursor(Cursor.CROSSHAIR);
+//
+//
+//        stage.setScene(scene);
+//
+//        stage.setOnCloseRequest((event) -> System.out.println("Closing Stage"));
+//        stage.show();
+
+
+
     }
 
     public static void main(String[] args) {
@@ -95,4 +121,21 @@ public class GUI extends Application implements View {
     public String getNickname() {
         return null;
     }
+
+    /* ACTION EVENTS */
+    public void setNicknameButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void connectButtonClicked(ActionEvent actionEvent) {
+        serverLabel.setOpacity(0);
+        serverTextField.setOpacity(0);
+        ipLabel.setOpacity(0);
+        ipTextField.setOpacity(0);
+        connectButton.setOpacity(0);
+
+        nicknameGroup.setOpacity(1);
+        setNicknameButton.setOpacity(1);
+
+    }
+
 }
