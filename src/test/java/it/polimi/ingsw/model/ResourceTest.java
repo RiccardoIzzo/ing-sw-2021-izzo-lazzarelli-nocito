865f35ccc23b87fd381c666ed3b1e6d0e9ad1d68
Andrea <andrea.nocito@mail.polimi.it>
2021-05-24 08:04:20 +0200

    add ResourceTest

---
+++ src/test/java/it/polimi/ingsw/model/ResourceTest.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.model;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * ResourceTest tests Resource class.
+ *
+ * @author Andrea Nocito
+ * */
+public class ResourceTest {
+
+    /**
+     * Method toStringTest gets the output of toString and checks that the result contains the desired symbol
+     * */
+    @Test
+    public void testToString() {
+        assertTrue(Resource.STONE.toString().contains("♠"));
+        assertTrue(Resource.SERVANT.toString().contains("♥"));
+        assertTrue(Resource.SHIELD.toString().contains("♣"));
+        assertTrue(Resource.COIN.toString().contains("♦"));
+    }
+}
