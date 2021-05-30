ed48dba04306d0a49567a39cea5326dec00485b9
GabrieleLazzarelli <gabriele.lazzarelli@mail.polimi.it>
2021-05-30 20:44:07 +0200

    update BuyCard

--- src/main/java/it/polimi/ingsw/events/clientmessages/BuyCard.java
+++ src/main/java/it/polimi/ingsw/events/clientmessages/BuyCard.java
@@ -6,15 +6,18 @@ package it.polimi.ingsw.events.clientmessages;
 public class BuyCard implements ClientMessage{
     private final int row;
     private final int column;
+    private final int index;
 
     /**
      * Constructor BuyCard creates a new BuyCard instance.
      * @param row row index of the card.
      * @param column column index of the card.
+     * @param index slot index in activeDevelopments.
      */
-    public BuyCard(int row, int column){
+    public BuyCard(int row, int column, int index){
         this.row = row;
         this.column = column;
+        this.index = index;
     }
 
     /**
@@ -31,5 +34,13 @@ public class BuyCard implements ClientMessage{
      */
     public int getColumn() {
         return column;
+    }
+
+    /**
+     * Method getIndex returns the slot index in activeDevelopments.
+     * @return the slot index where you want to place the card just bought.
+     */
+    public int getIndex() {
+        return index;
     }
 }
