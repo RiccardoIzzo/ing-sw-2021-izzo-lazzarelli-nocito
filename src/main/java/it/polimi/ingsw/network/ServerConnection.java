23aa6567f8bba3cc264659ca37ba2ff8ee180686
GabrieleLazzarelli <gabriele.lazzarelli@mail.polimi.it>
2021-06-02 20:33:58 +0200

    Update ServerConnection

--- src/main/java/it/polimi/ingsw/network/ServerConnection.java
+++ src/main/java/it/polimi/ingsw/network/ServerConnection.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.network;
 
-import it.polimi.ingsw.events.servermessages.*;
+import it.polimi.ingsw.events.servermessages.ServerMessage;
+import it.polimi.ingsw.events.servermessages.UpdateView;
 import it.polimi.ingsw.view.ActionHandler;
 import it.polimi.ingsw.view.ModelView;
 import it.polimi.ingsw.view.View;
@@ -8,17 +9,23 @@ import it.polimi.ingsw.view.View;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.stream.Collectors;
+
+import static it.polimi.ingsw.constants.GameConstants.END_TURN;
+import static it.polimi.ingsw.constants.GameConstants.TOKEN_DRAWN;
+import static it.polimi.ingsw.constants.PlayerConstants.*;
 
 /**
  * ServerConnection class manages inbound connection with the server.
  *
- * @author Riccardo Izzo
+ * @author Riccardo Izzo, Gabriele Lazzarelli
  */
 public class ServerConnection implements Runnable{
     private ObjectInputStream input;
     private boolean status;
+    ActionHandler actionHandler;
+    private final ArrayBlockingQueue<ServerMessage> messages;
     private final View view;
     private ModelView modelView;
 
@@ -29,6 +36,9 @@ public class ServerConnection implements Runnable{
      */
     public ServerConnection(Socket socket, View view){
         this.view = view;
+        this.messages = new ArrayBlockingQueue<>(10000);
+        this.actionHandler = new ActionHandler(view, messages);
+        actionHandler.start();
         try {
             input = new ObjectInputStream(socket.getInputStream());
         } catch (IOException e) {
@@ -60,19 +70,22 @@ public class ServerConnection implements Runnable{
      * Note that, to avoid conflicts during the model view update, only one ModelViewUpdater thread can be active.
      */
     public void receiveFromServer(){
+        ModelViewUpdater modelViewUpdater;
         try {
             ServerMessage message = (ServerMessage) input.readObject();
-            if(message instanceof UpdateView) {
-                ModelViewUpdater updater = new ModelViewUpdater(view, modelView, (UpdateView) message);
-                updater.start();
-                updater.join();
+            if (message instanceof UpdateView) {
+                modelViewUpdater = new ModelViewUpdater(modelView, (UpdateView) message);
+                modelViewUpdater.start();
+                if ((SET_LEADERS.equals(((UpdateView) message).getPropertyName()) || TEMPORARY_SHELF_CHANGE.equals(((UpdateView) message).getPropertyName()) || TOKEN_DRAWN.equals(((UpdateView) message).getPropertyName()) || END_TURN.equals(((UpdateView) message).getPropertyName()))){
+                    modelViewUpdater.join();
+                    messages.add(message);
+                }
             }
             else {
-                ActionHandler actionHandler = new ActionHandler(view, message);
-                actionHandler.start();
-                actionHandler.join();
+                messages.add(message);
             }
-        } catch (IOException | ClassNotFoundException | InterruptedException e) {
+        }
+        catch (IOException | ClassNotFoundException | InterruptedException e) {
             System.out.println("Can't find server! Quitting...");
             System.exit(0);
         }
