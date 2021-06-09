aae7e1b6c52ab370db71650168c4a3d382f96935
Andrea <andrea.nocito@mail.polimi.it>
2021-06-09 14:09:28 +0200

    update SelectLeaderController

--- src/main/java/it/polimi/ingsw/view/gui/SelectLeaderController.java
+++ src/main/java/it/polimi/ingsw/view/gui/SelectLeaderController.java
@@ -1,29 +1,30 @@
 package it.polimi.ingsw.view.gui;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.SelectionMode;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Map;
 import java.util.Set;
 
 public class SelectLeaderController {
-    public Button card1Button;
-    public ImageView card1ImageView;
-    public Button card2Button;
-    public ImageView card2ImageView;
-    public Button card3Button;
-    public ImageView card3ImageView;
-    public Button card4Button;
-    public ImageView card4ImageView;
-    public Button selectButton;
-    Set<Integer> leaderIds;
+    @FXML private Button card1Button;
+    @FXML private ImageView card1ImageView;
+    @FXML private Button card2Button;
+    @FXML private ImageView card2ImageView;
+    @FXML private Button card3Button;
+    @FXML private ImageView card3ImageView;
+    @FXML private Button card4Button;
+    @FXML private ImageView card4ImageView;
+    @FXML private Button selectButton;
+
+    static Set<Integer> leaderIds;
 
     private static GUI gui;
 
@@ -32,16 +33,24 @@ public class SelectLeaderController {
     }
 
 
+
     public void start() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/scenes/sceneSelectLeaders.fxml"));
         Parent root = loader.load();
-
+        loader.setController(this);
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.show();
-
+        showCards();
     }
+    public void showCards() {
 
+        int[] ids = leaderIds.stream().mapToInt(Number::intValue).toArray();
+        card1ImageView.setImage(new Image("/view/images/leaderCards/leaderCard"+ids[0]+".png"));
+        card2ImageView.setImage(new Image("/view/images/leaderCards/leaderCard"+ids[1]+".png"));
+        card3ImageView.setImage(new Image("/view/images/leaderCards/leaderCard"+ids[2]+".png"));
+        card4ImageView.setImage(new Image("/view/images/leaderCards/leaderCard"+ids[3]+".png"));
+    }
     public void setLeadersIds(Set<Integer> ids) {
         leaderIds = ids;
     }
