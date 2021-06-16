72ec02d81e62548b057576e8e95197b4d5389164
Andrea <andrea.nocito@mail.polimi.it>
2021-06-16 22:09:49 +0200

    add handleBonusResource method in DashboardController

--- src/main/java/it/polimi/ingsw/view/gui/DashboardController.java
+++ src/main/java/it/polimi/ingsw/view/gui/DashboardController.java
@@ -10,7 +10,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -29,6 +34,92 @@ public class DashboardController {
 
     public void setGUI(GUI gui) {
         DashboardController.gui = gui;
+    }
+
+    public void setup() {
+//        ImageView imageView = new ImageView(new Image(getClass().getResourceAsStream("BLUE.png")));
+
+    }
+    public void handleBonusResource(int amount) {
+        System.out.println("handleBonusResource " + amount);
+        ResourceMap bonusResources = new ResourceMap();
+        if(amount > 0) {
+            dashboardPane.setDisable(true);
+            Pane bonusPane = new Pane();
+            bonusPane.setPrefWidth(400);
+            bonusPane.setPrefHeight(300);
+            //add nodes
+            Label bonusTitle = new Label("Select " + amount + " Resource" + (amount > 1 ? "s" : ""));
+            bonusTitle.setLayoutX(100);
+            bonusTitle.setStyle("-fx-text-alignment: center ; -fx-font-size: 20px");
+            bonusTitle.setLayoutY(10);
+            bonusTitle.setPrefWidth(200);
+            bonusTitle.setPrefHeight(50);
+            bonusPane.getChildren().add(bonusTitle);
+
+            String[] resources = {"coin2", "servant2" , "shield2" , "stone2"};
+            TextField[] resourceTextField = new TextField[4];
+            for(int i = 0; i< resources.length; i++) {
+                Image resourceImage = new Image("/view/images/resources/"+resources[i]+".png");
+                ImageView resourceImageView = new ImageView(resourceImage);
+
+                resourceImageView.setLayoutX(100*i+20);
+                resourceImageView.setLayoutY(50);
+                resourceImageView.setFitWidth(60);
+                resourceImageView.setFitHeight(60);
+                resourceImageView.setStyle("-fx-border-width: 5");
+                bonusPane.getChildren().add(resourceImageView);
+
+                resourceTextField[i] = new TextField();
+                resourceTextField[i].setLayoutX(100*i+35);
+                resourceTextField[i].setLayoutY(115);
+                resourceTextField[i].setPrefWidth(30);
+                resourceTextField[i].setPrefHeight(15);
+                resourceTextField[i].setText("0");
+                int finalI = i;
+                resourceTextField[i].textProperty().addListener((observable, oldValue, newValue) -> {
+                    if (!newValue.matches("\\d*")) {
+                        resourceTextField[finalI].setText(newValue.replaceAll("[^\\d]", ""));
+                    }
+                    if (resourceTextField[finalI].getText().length() > 1) {
+                        resourceTextField[finalI].setText(String.valueOf(resourceTextField[finalI].getText().charAt(0)));
+                    }
+                });
+                bonusPane.getChildren().add(resourceTextField[i]);
+            }
+            Stage bonusStage = new Stage();
+
+            Button selectButton = new Button("Select");
+            selectButton.setLayoutX(150);
+            selectButton.setStyle("-fx-text-alignment: center ; -fx-font-size: 20px");
+            selectButton.setLayoutY(150);
+            selectButton.setPrefWidth(100);
+            selectButton.setPrefHeight(30);
+            selectButton.setOnAction(event -> {
+                int total = 0;
+                ResourceMap bonusResources1 = new ResourceMap();
+                for(int i=0; i<4; i++) {
+                    if (resourceTextField[i].getText().length() > 0) {
+                        total += Integer.parseInt(resourceTextField[i].getText());
+                        switch (i){
+                            case 0 -> bonusResources1.modifyResource(Resource.COIN, 1);
+                            case 1 -> bonusResources1.modifyResource(Resource.SERVANT, 1);
+                            case 2 -> bonusResources1.modifyResource(Resource.SHIELD, 1);
+                            case 3 -> bonusResources1.modifyResource(Resource.STONE, 1);
+                        }
+                    }
+                }
+                if(total == amount) {
+                    gui.send(new SendBonusResources(bonusResources1));
+                    bonusStage.close();
+                }
+            });
+            bonusPane.getChildren().add(selectButton);
+            Scene bonusScene = new Scene(bonusPane, 400, 200);
+            bonusStage.setTitle("Select bonus resources");
+            bonusStage.setScene(bonusScene);
+            bonusStage.show();
+        }
     }
 
     public void showMarket() {
