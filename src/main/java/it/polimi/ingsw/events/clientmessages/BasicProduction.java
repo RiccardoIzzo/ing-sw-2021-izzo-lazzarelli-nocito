bb1e78c1d53be4a6177cac562910f4943418072e
RIccardoIzzo <izzo.riccardo38@gmail.com>
2021-06-19 14:58:04 +0200

    add BasicProduction javadoc

--- src/main/java/it/polimi/ingsw/events/clientmessages/BasicProduction.java
+++ src/main/java/it/polimi/ingsw/events/clientmessages/BasicProduction.java
@@ -2,19 +2,35 @@ package it.polimi.ingsw.events.clientmessages;
 
 import it.polimi.ingsw.model.ResourceMap;
 
+/**
+ * BasicProduction message is used to activate the basic production.
+ */
 public class BasicProduction implements ClientMessage {
-    ResourceMap inputResources;
-    ResourceMap outputResources;
+    private final ResourceMap inputResources;
+    private final ResourceMap outputResources;
 
+    /**
+     * Constructor BasicProduction creates a new BasicProduction instance.
+     * @param inputResources input resources for the basic production.
+     * @param outputResources output resource for the basic production.
+     */
     public BasicProduction(ResourceMap inputResources, ResourceMap outputResources) {
         this.inputResources = inputResources;
         this.outputResources = outputResources;
     }
 
+    /**
+     * Method getInputProduction returns the input resources.
+     * @return a ResourceMap with the input resources.
+     */
     public ResourceMap getInputProduction() {
         return inputResources;
     }
 
+    /**
+     * Method getOutputProduction returns the output resources.
+     * @return a ResourceMap with the output resources.
+     */
     public ResourceMap getOutputProduction() {
         return outputResources;
     }
