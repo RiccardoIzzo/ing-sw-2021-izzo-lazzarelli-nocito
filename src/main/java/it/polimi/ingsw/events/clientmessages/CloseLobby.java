09ed0c372be26d894ac8a281033c5c2ea726b521
RIccardoIzzo <izzo.riccardo38@gmail.com>
2021-06-30 21:26:43 +0200

    add CloseLobby message

---
+++ src/main/java/it/polimi/ingsw/events/clientmessages/CloseLobby.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.events.clientmessages;
+
+/**
+ * CloseLobby message is used by the client to close the lobby at the end of the game.
+ */
+public class CloseLobby implements ClientMessage{}
